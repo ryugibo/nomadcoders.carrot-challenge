2af794277181011e63931ccce1e02660aa6b6c53
ryugibo <7928632+ryugibo@users.noreply.github.com>
2025-02-01 00:22:07 +0900

    Assignment 07. zod 사용한 폼 입력 검증

--- urban-telegram/app/page.tsx
+++ urban-telegram/app/page.tsx
@@ -12,22 +12,32 @@ import { useActionState } from "react";
 import { onSubmit } from "./action";
 import { CheckCircleIcon } from "@heroicons/react/24/outline";
 
+const initialState = {
+  requested: false,
+  errors: undefined,
+};
+
 export default function Home() {
-  const [state, action] = useActionState(onSubmit, null);
+  const [state, action] = useActionState(onSubmit, initialState);
   return (
     <div className="flex flex-col max-w-sm mx-auto h-screen justify-center">
       <div className="text-red-500 size-16 mb-16 flex w-full">
         <FireIcon />
       </div>
       <form action={action} className="flex flex-col gap-3">
-        <FormInput name="email" type="email" placeholder="Email" errors={[]}>
+        <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          errors={state.errors?.fieldErrors.email}
+        >
           <EnvelopeIcon className="size-4" />
         </FormInput>
         <FormInput
           name="username"
           type="text"
           placeholder="Username"
-          errors={[]}
+          errors={state.errors?.fieldErrors.username}
         >
           <UserIcon className="size-4" />
         </FormInput>
@@ -35,18 +45,18 @@ export default function Home() {
           name="password"
           type="password"
           placeholder="Password"
-          errors={state?.errors ?? []}
+          errors={state.errors?.fieldErrors.password}
         >
           <KeyIcon className="size-4" />
         </FormInput>
         <FormButton text="Log in" />
       </form>
-      {state?.ok ? (
+      {!state.requested ? null : state.errors ? null : (
         <div className="bg-green-500 h-14 flex items-center px-2 rounded-2xl mt-3">
           <CheckCircleIcon className="size-6 mr-4" />
           Welcome back!
         </div>
-      ) : null}
+      )}
     </div>
   );
 }
