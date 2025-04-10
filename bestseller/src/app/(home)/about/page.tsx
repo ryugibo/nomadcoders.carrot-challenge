f4e69bd9608c0d5eb7cedbffd4ef2d682f8ec98c
ryugibo <7928632+ryugibo@users.noreply.github.com>
2025-04-10 16:26:07 +0900

    about 페이지 스타일

--- bestseller/src/app/(home)/about/page.tsx
+++ bestseller/src/app/(home)/about/page.tsx
@@ -1,12 +1,14 @@
+import styles from "@/styles/about.module.css";
+
 export default function About() {
   return (
-    <div>
-      <header>ABOUT US</header>
+    <div className={styles.container}>
+      <header className={styles.title}>ABOUT US</header>
       <p>
         Welcome to the official explorer for The New York Times Best Seller list
         explorer.
       </p>
-      <p>We hope you enjoy your stay!</p>
+      <p> We hope you enjoy your stay!</p>
     </div>
   );
 }
