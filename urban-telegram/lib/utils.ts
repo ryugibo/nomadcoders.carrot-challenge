aa2596de0b02c18613ab24af230d329c2f4da413
ryugibo <7928632+ryugibo@users.noreply.github.com>
2025-04-22 23:46:41 +0900

    기존 코드 제거

--- urban-telegram/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-export function formatToTimeAgo(date: Date): string {
-  const dayInMs = 1000 * 60 * 60 * 24;
-  const time = date.getTime();
-  const now = new Date().getTime();
-  const diff = Math.round((time - now) / (1000 * 60));
-  const formatter = new Intl.RelativeTimeFormat("ko");
-  return formatter.format(diff, "minutes");
-}
