ca46bd4b1c007ff65ea7f63e79935b1d085d399f
ryugibo <7928632+ryugibo@users.noreply.github.com>
2025-05-09 22:55:13 +0900

    Link 중첩 에러 수정

--- tweest/src/components/list-tweet.tsx
+++ tweest/src/components/list-tweet.tsx
@@ -14,18 +14,18 @@ export default function ListTweet({
   username,
 }: ListTweetProps) {
   return (
-    <Link href={`/tweets/${id}`}>
-      <div className="border-2 w-full border-neutral-800 py-3">
-        <div className="flex flex-row justify-between mx-3">
-          <Link href={`/users/${username}`}>
-            <div className="hover:underline">{username}</div>
-          </Link>
-          <div>{formatToTimeAgo(created_at)}</div>
-        </div>
+    <div className="border-2 w-full border-neutral-800 py-3">
+      <div className="flex flex-row justify-between mx-3">
+        <Link href={`/users/${username}`}>
+          <div className="hover:underline">{username}</div>
+        </Link>
+        <div>{formatToTimeAgo(created_at)}</div>
+      </div>
+      <Link href={`/tweets/${id}`}>
         <div className=" bg-neutral-600 rounded-md py-4 px-2 mx-2 text-white">
           {tweet}
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
